456f87d12cfadc5050392a7d2262705d22db9fe8
Abdur Rahman <159477884+abdurrahmancce@users.noreply.github.com>
2025-08-30 08:07:15 -0500

    Update Digital Clock.py

--- Digital Clock.py
+++ Digital Clock.py
@@ -1,16 +1,16 @@
 import datetime
 import time
-import winsound  # For Windows sound
+import winsound 
 
 while True:
     
     print("Current Date and Time:", datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
 
-    # Every 5 seconds, play a beep sound
 
-    if int(datetime.datetime.now().strftime("%S")) % 5 == 0:  # Display current time
+    if int(datetime.datetime.now().strftime("%S")) % 5 == 0:  
 
-        winsound.Beep(1000, 500)  # frequency=1000Hz, duration=500ms
+        winsound.Beep(1000, 500) 
 
-    time.sleep(1)  # Wait for 1 second
+    time.sleep(1) 
+
 
